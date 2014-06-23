063a6ccf73e7e7d99796176be94d2afe10489256
Lucas Simao <lsimaocosta@gmail.com>
2014-06-23 15:02:34 -0300

    query de estoque aprimorada

--- grails-app/controllers/br/com/arrasaamiga/EstoqueController.groovy
+++ grails-app/controllers/br/com/arrasaamiga/EstoqueController.groovy
@@ -22,9 +22,7 @@ class EstoqueController {
         def results = c.list () {
 
             produto{
-                and{
-                    eq('visivel',true) 
-                }
+                eq('visivel',true) 
                 order('nome')
             }
 
