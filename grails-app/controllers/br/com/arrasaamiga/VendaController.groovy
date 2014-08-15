ae75c3160190a3c12cc9ec2009d3b694d7e19d64
Lucas Simao <lsimaocosta@gmail.com>
2014-08-15 10:42:34 -0300

    correcao da query que retorna as vendas em formato JSON. Agora so retorna vendas que foram corretamente finalizadas

--- grails-app/controllers/br/com/arrasaamiga/VendaController.groovy
+++ grails-app/controllers/br/com/arrasaamiga/VendaController.groovy
@@ -17,24 +17,24 @@ class VendaController extends RestfulController {
         super(Venda)
     }
 
-    
+
     @Secured(['isAuthenticated()'])
-    def show(Long id){
+    def show(Long id) {
 
         def venda = Venda.get(id)
 
-        withFormat{
-            html { 
-                if (venda){
-                    [numeroPedido: String.format("%05d", venda.id) , venda : venda,
-                        enderecoEntrega: venda.cliente.endereco,cliente: venda.cliente]
+        withFormat {
+            html {
+                if (venda) {
+                    [numeroPedido   : String.format("%05d", venda.id), venda: venda,
+                     enderecoEntrega: venda.cliente.endereco, cliente: venda.cliente]
 
-                }else{
+                } else {
                     println "Erro ao carregar venda ${id}"
                     render 'Venda não encontrada'
                 }
             }
-            json { 
+            json {
                 if (venda)
                     respond venda
                 else
@@ -47,36 +47,37 @@ class VendaController extends RestfulController {
     /* 
      *   TODO verificar se a nova versão corrigiu o bug do metodo update
      */
-    def update(){
+
+    def update() {
 
         def venda = Venda.get(params.id)
         venda.properties = params
 
-        if (venda.hasErrors()){
+        if (venda.hasErrors()) {
             println venda.errors
             respond venda.errors
 
-        }else{
-            venda.save(flush:true)
-            render status:OK
-            
+        } else {
+            venda.save(flush: true)
+            render status: OK
+
         }
 
     }
 
-    def save(){
-        request.withFormat{
+    def save() {
+        request.withFormat {
 
-            json{
+            json {
                 def json = request.JSON
 
                 def venda = new Venda()
                 venda.dataEntrega = new Date(json.dataEntrega)
                 venda.cliente = new Cliente(json.cliente)
-                venda.cliente.endereco.cidade= Cidade.teresina
+                venda.cliente.endereco.cidade = Cidade.teresina
                 venda.cliente.endereco.uf = Uf.piaui
                 venda.vendedor = Usuario.findByUsername(json.vendedor)
-                venda.formaPagamento =  FormaPagamento.valueOf(json.formaPagamento)
+                venda.formaPagamento = FormaPagamento.valueOf(json.formaPagamento)
 
                 if (venda.formaPagamento == FormaPagamento.JaPagou)
                     venda.status = StatusVenda.PagamentoRecebido
@@ -84,16 +85,16 @@ class VendaController extends RestfulController {
                     venda.status = StatusVenda.AguardandoPagamento
 
 
-                JSON.parse(json.itens).each{obj->
+                JSON.parse(json.itens).each { obj ->
                     def estoque = Estoque.get(obj.estoqueId)
                     shoppingCartService.addToShoppingCart(estoque.produto, estoque.unidade, obj.quantidade)
                 }
 
-                venda.carrinho = shoppingCartService.shoppingCart  
-                venda.carrinho.checkedOut = true              
+                venda.carrinho = shoppingCartService.shoppingCart
+                venda.carrinho.checkedOut = true
 
-                venda.save(flush:true)
-                render status: OK               
+                venda.save(flush: true)
+                render status: OK
             }
         }
 
@@ -101,29 +102,29 @@ class VendaController extends RestfulController {
 
 
     @Secured(['IS_AUTHENTICATED_FULLY'])
-    def setTrackingCode(Long id,String trackingCode){
-            
-        if (trackingCode){
+    def setTrackingCode(Long id, String trackingCode) {
+
+        if (trackingCode) {
             def v = Venda.load(id)
             v.codigoRastreio = trackingCode
-            v.save(flush:true)
+            v.save(flush: true)
             flash.message = 'Código de rastreio atualizado com sucesso'
-        }       
+        }
 
-        redirect(action:'list', params:[offset:params.offset, max: params.max] )
+        redirect(action: 'list', params: [offset: params.offset, max: params.max])
 
     }
 
 
     @Secured(['IS_AUTHENTICATED_FULLY'])
-    def showFull(Long id){
+    def showFull(Long id) {
 
         def venda = Venda.get(id)
 
-        if (venda){
-            [ numeroPedido: String.format("%05d", venda.id) , venda : venda,cliente: venda.cliente]
+        if (venda) {
+            [numeroPedido: String.format("%05d", venda.id), venda: venda, cliente: venda.cliente]
 
-        }else{
+        } else {
             println "Erro ao carregar venda ${id}"
             render 'Venda não encontrada'
         }
@@ -131,60 +132,60 @@ class VendaController extends RestfulController {
     }
 
     @Secured(['IS_AUTHENTICATED_FULLY'])
-    def marcarComoEntregue(Long id){
+    def marcarComoEntregue(Long id) {
         def v = Venda.get(id)
         v.status = StatusVenda.Entregue
 
-        v.save(flush:true)
+        v.save(flush: true)
 
         flash.message = 'A venda foi marcada como entregue'
-        
-        if (params.offset && params.max){
-            redirect(action:'list', params:[offset:params.offset, max: params.max] )
-        }else{
-            redirect(action:'list')
+
+        if (params.offset && params.max) {
+            redirect(action: 'list', params: [offset: params.offset, max: params.max])
+        } else {
+            redirect(action: 'list')
         }
-            
+
 
     }
 
     //@Secured(['IS_AUTHENTICATED_FULLY'])
-    def excluir(Long id){
+    def excluir(Long id) {
 
         def v = Venda.load(id)
-        v.delete(flush:true)
+        v.delete(flush: true)
         flash.message = 'A venda foi excluida'
 
 
-        withFormat{
-            html { 
+        withFormat {
+            html {
 
                 if (params.offset && params.max)
-                    redirect(action:'list', params:[offset:params.offset, max: params.max] )
+                    redirect(action: 'list', params: [offset: params.offset, max: params.max])
                 else
-                    redirect(action:'list')
+                    redirect(action: 'list')
 
             }
-            json { 
+            json {
                 render status: OK
             }
-        }            
-            
+        }
+
     }
 
 
-    def cancelada(){
+    def cancelada() {
 
     }
 
     def index() {
-        withFormat{
+        withFormat {
             html { redirect(action: "list", params: params) }
-            json { 
-                respond Venda.findAllByStatusNotEqual(StatusVenda.Entregue) 
+            json {
+                respond Venda.findAllByStatusInList([StatusVenda.AguardandoPagamento, StatusVenda.PagamentoRecebido])
             }
         }
-        
+
     }
 
     @Secured(['IS_AUTHENTICATED_FULLY'])
@@ -192,7 +193,7 @@ class VendaController extends RestfulController {
         params.max = Math.min(max ?: 10, 100)
         params.sort = 'dateCreated'
         params.order = 'desc'
-        respond Venda.list(params), model:[vendaInstanceTotal: Venda.count()]
+        respond Venda.list(params), model: [vendaInstanceTotal: Venda.count()]
     }
 
 
