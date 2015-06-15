21ee74adafe6d98ba6b416041708cef710f54775
Lucas Simão <lsimaocosta@gmail.com>
2015-06-14 23:16:52 -0300

    protegendo a action ProdutoController#quantidadeEmEstoque de requisições maliciosas

--- grails-app/controllers/br/com/arrasaamiga/ProdutoController.groovy
+++ grails-app/controllers/br/com/arrasaamiga/ProdutoController.groovy
@@ -15,37 +15,39 @@ import grails.util.BuildSettingsHolder
 import groovy.json.JsonBuilder
 import grails.util.Environment
 
+import static org.springframework.http.HttpStatus.NOT_FOUND
+
 
 @Secured(['permitAll'])
 class ProdutoController {
 
     static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
-    
+
     def grailsApplication
     def springSecurityService
     def dataSource
 
-    def getTags(){
+    def getTags() {
         def sql = new Sql(dataSource)
 
-        
+
         String searchKey = "%${params.term}%"
         def rows = sql.rows("select distinct pw.keywords_string from produto_keywords pw where lower(pw.keywords_string) like lower(:search)",
-                            [search:searchKey])
+                [search: searchKey])
 
         def tags = [:]
 
-        rows.eachWithIndex{row, index-> tags[index] = row.keywords_string }
+        rows.eachWithIndex { row, index -> tags[index] = row.keywords_string }
         render tags as JSON
     }
 
-    private String getGruposDeProdutos(){
+    private String getGruposDeProdutos() {
         def grupos = []
         def builder = new JsonBuilder()
 
-        
-        GrupoDeProduto.list().each{grupo->
-            grupos<< ['id': grupo.id,'label': grupo.nome]
+
+        GrupoDeProduto.list().each { grupo ->
+            grupos << ['id': grupo.id, 'label': grupo.nome]
         }
 
         builder(grupos)
@@ -54,11 +56,11 @@ class ProdutoController {
     }
 
 
-    def addNewUnidade(String unidade){
-        render(template:'addNewUnidade',model:[unidade:unidade])
+    def addNewUnidade(String unidade) {
+        render(template: 'addNewUnidade', model: [unidade: unidade])
     }
 
-    def asyncFotoUpload(){
+    def asyncFotoUpload() {
 
         String uploadDir = getUploadDir()
 
@@ -71,7 +73,7 @@ class ProdutoController {
             def foto = new FotoProduto(arquivo: originalFilename)
 
             render(template: 'addNewFoto', model: [foto: foto])
-        }else{
+        } else {
             render status: 400, text: 'A imagem deve ser enviada'
         }
     }
@@ -85,9 +87,9 @@ class ProdutoController {
     @Secured(['ROLE_ADMIN'])
     def list(Integer max) {
         params.max = Math.min(max ?: 10, 100)
-        
-        def produtoInstanceList = Produto.executeQuery('from Produto p where p.foraDeLinha=false order by ordem asc',[],params)
-        respond produtoInstanceList , model: [produtoInstanceTotal: Produto.count()]
+
+        def produtoInstanceList = Produto.executeQuery('from Produto p where p.foraDeLinha=false order by ordem asc', [], params)
+        respond produtoInstanceList, model: [produtoInstanceTotal: Produto.count()]
 
     }
 
@@ -96,24 +98,23 @@ class ProdutoController {
         def produtoInstance = new Produto(params)
         produtoInstance.visivel = true
 
-        [produtoInstance: produtoInstance,gruposDeProdutos: getGruposDeProdutos()]
+        [produtoInstance: produtoInstance, gruposDeProdutos: getGruposDeProdutos()]
     }
 
     @Secured(['ROLE_ADMIN'])
     def save() {
 
-        def unidades = (params.unidades)?JSON.parse(params.unidades):[]
-        def fotos = (params.fotosUnidades)?JSON.parse(params.fotosUnidades):[]
-        def comentarios = (params.fotoComentario)?JSON.parse(params.fotoComentario):[]
-
+        def unidades = (params.unidades) ? JSON.parse(params.unidades) : []
+        def fotos = (params.fotosUnidades) ? JSON.parse(params.fotosUnidades) : []
+        def comentarios = (params.fotoComentario) ? JSON.parse(params.fotoComentario) : []
 
         // corrigindo os grupos
         def pattern = ~/_grupos\[\d+\-a\]/
-        def paramsGroups = params.keySet().findAll{p->  return (pattern.matcher(p).matches()) }
+        def paramsGroups = params.keySet().findAll { p -> return (pattern.matcher(p).matches()) }
 
         int count = 0
         def pattern2 = ~/\d+/
-        paramsGroups.each{p->
+        paramsGroups.each { p ->
             params["grupos[${count}].id"] = pattern2.matcher(p)[0]
             ++count
         }
@@ -124,12 +125,12 @@ class ProdutoController {
         produtoInstance.fotos = []
         produtoInstance.unidades = []
 
-        unidades.each{ unidade->
+        unidades.each { unidade ->
             produtoInstance.addToUnidades(unidade.trim())
 
-            fotos[unidade].eachWithIndex{ foto, index ->
+            fotos[unidade].eachWithIndex { foto, index ->
                 def fotoProduto = new FotoProduto();
-                
+
                 fotoProduto.comentario = comentarios[foto]?.trim();
                 fotoProduto.posicao = index
                 fotoProduto.unidade = unidade.trim()
@@ -138,7 +139,7 @@ class ProdutoController {
                 produtoInstance.addToFotos(fotoProduto)
             }
         }
-        
+
         def multipartFileMiniatura = request.getFile('fotoMiniaturaFile')
         if (multipartFileMiniatura)
             produtoInstance.fotoMiniatura = "img${System.currentTimeMillis()}${multipartFileMiniatura.originalFilename}"
@@ -156,7 +157,7 @@ class ProdutoController {
             multipartFileMiniatura.transferTo(new File(uploadDir + File.separator + produtoInstance.fotoMiniatura))
 
 
-       produtoInstance.unidades.each{ un->
+        produtoInstance.unidades.each { un ->
 
             def estoque = new Estoque()
 
@@ -164,14 +165,14 @@ class ProdutoController {
             estoque.unidade = un
             estoque.quantidade = 0 // qtde inicial
 
-            estoque.save(flush:true)
+            estoque.save(flush: true)
 
         }
 
 
         flash.message = "Produto cadastrado"
         redirect(action: "show", id: produtoInstance.id)
-        
+
     }
 
     //Apresenta detalhes do produto ao administrador
@@ -188,7 +189,6 @@ class ProdutoController {
     }
 
 
-
     @Secured(['ROLE_ADMIN'])
     def edit(Long id) {
         def produtoInstance = Produto.get(id)
@@ -198,9 +198,8 @@ class ProdutoController {
             return
         }
 
-        [produtoInstance: produtoInstance,gruposDeProdutos: getGruposDeProdutos()]
+        [produtoInstance: produtoInstance, gruposDeProdutos: getGruposDeProdutos()]
     }
-
 
 
     @Secured(['ROLE_ADMIN'])
@@ -216,8 +215,8 @@ class ProdutoController {
         if (version != null) {
             if (produtoInstance.version > version) {
                 produtoInstance.errors.rejectValue("version", "default.optimistic.locking.failure",
-                          [message(code: 'produto.label', default: 'Produto')] as Object[],
-                          "Another user has updated this Produto while you were editing")
+                        [message(code: 'produto.label', default: 'Produto')] as Object[],
+                        "Another user has updated this Produto while you were editing")
                 render(view: "edit", model: [produtoInstance: produtoInstance])
                 return
             }
@@ -225,29 +224,28 @@ class ProdutoController {
 
 
         produtoInstance.keywords = params.list('palavrasChave[]')
-        
+
         // adicionando novas tags
         def patternKeywords = ~/palavrasChave\[\d+\-a\]/
-        def paramsTags = params.keySet().findAll{p->  return (patternKeywords.matcher(p).matches()) }
-        paramsTags.each{ key->
+        def paramsTags = params.keySet().findAll { p -> return (patternKeywords.matcher(p).matches()) }
+        paramsTags.each { key ->
             produtoInstance.keywords << params[key]
         }
 
-
         // atualizando os grupos
         def patternGrupos = ~/_grupos\[\d+\-a\]/
-        def paramsGroups = params.keySet().findAll{p->  return (patternGrupos.matcher(p).matches()) }
+        def paramsGroups = params.keySet().findAll { p -> return (patternGrupos.matcher(p).matches()) }
 
         int count = 0
         def pattern2 = ~/\d+/
-        paramsGroups.each{p->
+        paramsGroups.each { p ->
             params["grupos[${count}].id"] = pattern2.matcher(p)[0]
             ++count
         }
 
         produtoInstance.grupos = []
-        bindData(produtoInstance, params, [include: ['grupos','visivel','nome','descricao',
-                                                     'tipoUnitario','marca','precoAVistaEmReais','precoAPrazoEmReais']])
+        bindData(produtoInstance, params, [include: ['grupos', 'visivel', 'nome', 'descricao',
+                                                     'tipoUnitario', 'marca', 'precoAVistaEmReais', 'precoAPrazoEmReais']])
         assert count == produtoInstance.grupos?.size()
 
         def miniaturaAnterior = produtoInstance.fotoMiniatura
@@ -255,44 +253,44 @@ class ProdutoController {
 
 
         if (multipartFileMiniatura?.originalFilename)
-            produtoInstance.fotoMiniatura =  multipartFileMiniatura.originalFilename
+            produtoInstance.fotoMiniatura = multipartFileMiniatura.originalFilename
         else
-             produtoInstance.fotoMiniatura = miniaturaAnterior
+            produtoInstance.fotoMiniatura = miniaturaAnterior
 
 
         produtoInstance.fotos*.delete()
         produtoInstance.fotos?.clear()
         produtoInstance.unidades.clear()
 
-        def unidades = (params.unidades)?JSON.parse(params.unidades):[]
-        def fotos = (params.fotosUnidades)?JSON.parse(params.fotosUnidades):[]
-        def comentarios = (params.fotoComentario)?JSON.parse(params.fotoComentario):[]
+        def unidades = (params.unidades) ? JSON.parse(params.unidades) : []
+        def fotos = (params.fotosUnidades) ? JSON.parse(params.fotosUnidades) : []
+        def comentarios = (params.fotoComentario) ? JSON.parse(params.fotoComentario) : []
 
 
 
-        unidades.each{ unidade ->
+        unidades.each { unidade ->
 
-            if (!Estoque.findByProdutoAndUnidade( produtoInstance, unidade ) ){
-                
+            if (!Estoque.findByProdutoAndUnidade(produtoInstance, unidade)) {
+
                 def estoque = new Estoque()
 
                 estoque.produto = produtoInstance
                 estoque.unidade = unidade
                 estoque.quantidade = 0 // qtde inicial
 
-                estoque.save(flush:true)
+                estoque.save(flush: true)
             }
 
             produtoInstance.addToUnidades(unidade)
 
-            fotos[unidade].eachWithIndex{ arquivoFoto, index ->
+            fotos[unidade].eachWithIndex { arquivoFoto, index ->
 
                 def fotoProduto = new FotoProduto()
-                
+
                 fotoProduto.comentario = comentarios[arquivoFoto]?.trim()
                 fotoProduto.posicao = index
                 fotoProduto.unidade = unidade
-                fotoProduto.arquivo = arquivoFoto    
+                fotoProduto.arquivo = arquivoFoto
 
                 produtoInstance.addToFotos(fotoProduto)
 
@@ -307,14 +305,14 @@ class ProdutoController {
         }
 
         // removendo estoques que nao possuem mais
-        Estoque.findAllByProduto(produtoInstance)?.each{e->
-            if (!produtoInstance.unidades.contains(e.unidade)){
-                e.delete(flush:true)
+        Estoque.findAllByProduto(produtoInstance)?.each { e ->
+            if (!produtoInstance.unidades.contains(e.unidade)) {
+                e.delete(flush: true)
             }
         }
 
-        if (multipartFileMiniatura?.originalFilename){
-            String uploadDir =  getUploadDir()
+        if (multipartFileMiniatura?.originalFilename) {
+            String uploadDir = getUploadDir()
             multipartFileMiniatura.transferTo(new File(uploadDir + File.separator + produtoInstance.fotoMiniatura))
         }
 
@@ -343,37 +341,34 @@ class ProdutoController {
         }
     }
 
-
-
     //Apresenta detalhes do produto ao comprador
     def detalhes(Long id) {
         def produtoInstance = Produto.get(id)
         def user = springSecurityService.currentUser
         def cliente = Cliente.findByUsuario(user)
-        
+
         if (!produtoInstance) {
             flash.message = message(code: 'default.not.found.message', args: [message(code: 'produto.label', default: 'Produto'), id])
-            redirect(uri: "/", absolute:true)
+            redirect(uri: "/", absolute: true)
             return
         }
 
         def estoques = produtoInstance.getEstoques()
         // ordenando em ordem decrescente de quantidade
-        List unidades = estoques.sort{ e1, e2-> e2.quantidade <=> e1.quantidade }.collect{ it.unidade } 
+        List unidades = estoques.sort { e1, e2 -> e2.quantidade <=> e1.quantidade }.collect { it.unidade }
 
-        [produtoInstance: produtoInstance,unidades: unidades,
-            shoppingCart: session.shoppingCart,
-            estoques: estoques,cliente:cliente, grupoRaiz: produtoInstance.grupoPadrao?.getGrupoRaiz()?.nome]
+        [produtoInstance: produtoInstance, unidades: unidades,
+         shoppingCart   : session.shoppingCart,
+         estoques       : estoques, cliente: cliente, grupoRaiz: produtoInstance.grupoPadrao?.getGrupoRaiz()?.nome]
     }
 
 
-    def salvarAviso(Long id){
+    def salvarAviso(Long id) {
 
-        if (!params.signed_request){
-            render status:org.springframework.http.HttpStatus.BAD_REQUEST
+        if (!params.signed_request) {
+            render status: org.springframework.http.HttpStatus.BAD_REQUEST
             return
         }
-
 
         //Facebook App info
         String fbSecretKey;
@@ -390,9 +385,7 @@ class ProdutoController {
                 fbAppId = "592257150816024"
         }
 
-
-
-        //it is important to enable url-safe mode for Base64 encoder 
+        //it is important to enable url-safe mode for Base64 encoder
         Base64 base64 = new Base64(true);
 
         //split request into signature and data
@@ -406,14 +399,14 @@ class ProdutoController {
         def produto = Produto.load(data.registration.idProduto)
 
         //check signature algorithm
-        if(!data.algorithm.equals("HMAC-SHA256")) {
+        if (!data.algorithm.equals("HMAC-SHA256")) {
             flash.info = 'Ocorreu um erro ao salvar seu aviso, entre em contato com a gente!'
             redirect(uri: produto.nomeAsURL)
             return null
         }
 
         //check if data is signed correctly
-        if(!hmacSHA256(signedRequest[1], fbSecretKey).equals(sig)) {
+        if (!hmacSHA256(signedRequest[1], fbSecretKey).equals(sig)) {
             flash.info = 'Ocorreu um erro ao salvar seu aviso, entre em contato com a gente!'
             redirect(uri: produto.nomeAsURL)
             return null
@@ -430,10 +423,10 @@ class ProdutoController {
         aviso.unidade = data.registration.unidade
         aviso.facebookUserId = data.user_id
 
-        aviso.save(flush:true)
+        aviso.save(flush: true)
 
         flash.info = 'Amiga, avisaremos você assim que novas unidades chegarem !!'
-        redirect(uri:aviso.produto.nomeAsURL)
+        redirect(uri: aviso.produto.nomeAsURL)
 
     }
 
@@ -447,53 +440,46 @@ class ProdutoController {
     }
 
 
-
-
-    def quantidadeEmEstoque(Long produtoId,String unidade) {
+    def quantidadeEmEstoque(Long produtoId, String unidade) {
 
         def produtoInstance = Produto.get(produtoId)
-        
+
         if (!produtoInstance) {
-            render "erro"
-            throw new Exception("Erro ao carregar produto com id ${produtoId} em ProdutoController#quantidadeEmEstoque")
+            render status: NOT_FOUND
+            log.info("Erro ao carregar produto com id ${produtoId} em ProdutoController#quantidadeEmEstoque")
+            return
         }
 
-        if (!unidade){
-            render "erro"
-            throw new Exception("Alguem tentanto carregar o produto ${produtoId} sem unidade ....")
+        if (!unidade) {
+            render status: NOT_FOUND
+            log.info("Alguem tentanto carregar o produto ${produtoId} sem unidade ....")
+            return
         }
 
-        if (!produtoInstance.unidades.contains(unidade)){
-            render "erro"
-            throw new Exception("Não existe a unidade ${params.unidade} no produto ${produtoId} ....")
+        if (!produtoInstance.unidades.contains(unidade)) {
+            render status: NOT_FOUND
+            log.info("Não existe a unidade ${params.unidade} no produto ${produtoId} ....")
+            return
         }
 
-        try{
-            def quantidade = produtoInstance.getQuantidadeEmEstoque(unidade)
+        def quantidade = produtoInstance.getQuantidadeEmEstoque(unidade)
 
-            boolean marcadoParaAvisar = false
+        boolean marcadoParaAvisar = false
 
-            if (quantidade == 0){
-                def user = springSecurityService.currentUser
-                
-                if (user){
-                    def cliente = Cliente.findByUsuario(user)
-                    marcadoParaAvisar = ( Aviso.findByProdutoAndEmailAndUnidade(produtoInstance,cliente.email,unidade) != null )
-                }
+        if (quantidade == 0) {
+            def user = springSecurityService.currentUser
+
+            if (user) {
+                def cliente = Cliente.findByUsuario(user)
+                marcadoParaAvisar = (Aviso.findByProdutoAndEmailAndUnidade(produtoInstance, cliente.email, unidade) != null)
             }
-
-            render (['quantidade': quantidade, 'marcadoParaAvisar': marcadoParaAvisar] as JSON)
-
-        }catch(Exception e){
-            render "erro"
-            throw e
         }
-       
-         
+
+        render(['quantidade': quantidade, 'marcadoParaAvisar': marcadoParaAvisar] as JSON)
+
     }
 
-
-    private String getUploadDir(){
+    private String getUploadDir() {
         return grailsApplication.mainContext.getResource('images/produtos').file.absolutePath
     }
 
